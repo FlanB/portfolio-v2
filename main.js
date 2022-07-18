2fed1d0242c1f3716f1947a73153b70af4c0c99b
FlanB <clement.renou@mmibordeaux.com>
2022-07-18 12:10:14 +0200

    ✨ (main.js) create cube

--- main.js
+++ main.js
@@ -1,23 +1,54 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "./style.css"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//CANVAS
+const canvas = document.querySelector("#webgl")
 
-setupCounter(document.querySelector('#counter'))
+//SIZES
+const sizes = {
+	width: window.innerWidth, height: window.innerHeight
+}
+
+addEventListener("resize", () => {
+	sizes.width = window.innerWidth
+	sizes.height = window.innerHeight
+	renderer.setSize(sizes.width, sizes.height)
+	camera.aspect = sizes.width / sizes.height
+	camera.updateProjectionMatrix()
+})
+
+//SCENE
+const scene = new THREE.Scene()
+
+//CAMERA
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 5
+
+scene.add(camera)
+
+//OBJECTS
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+
+//RENDERER
+const renderer = new THREE.WebGLRenderer({
+	canvas, antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+//CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.update()
+
+//ANIMATION
+const animate = () => {
+	requestAnimationFrame(animate)
+	// cube.rotation.x += 0.01
+	// cube.rotation.y += 0.01
+	renderer.render(scene, camera)
+}
+animate()
